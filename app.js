cf9f2cac16e8b87131fd2e3d356f347c5fb1719d
moms-spaghetti <williamedwards36@aol.com>
2021-01-21 19:44:23 +0000

    port changes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = process.env.EXPRESS_PORT;
+const port = process.env.PORT;
 const {
   dbInsertQuote,
   dbGetQuoteList,
