3c4e2a45ac5afca632af8bbbe83b577d82bd961e
moms-spaghetti <williamedwards36@aol.com>
2021-01-21 17:20:02 +0000

    adjust enviroment variables

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = 80;
+const port = process.env.EXPRESS_PORT;
 const {
   dbInsertQuote,
   dbGetQuoteList,
